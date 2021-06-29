9690ccb1c300d989c808095c5ad214cbea34c71d
Max <mikimax79@mail.ru>
2021-06-29 17:24:23 +0300

    Third commit

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,11 @@
 public class MyClass {
     public void main(String args[]){
         int a = 5;
+        a=multiply(5,6);
+        System.out.println("The result of multiplication "+a);
+    }
+
+    public int multiply(int a,int b){
+        return a*b;
     }
 }
